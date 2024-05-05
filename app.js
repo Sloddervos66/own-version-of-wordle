1760a21b6f75ca6811a29b761ca0e45ac7781cda
Sloddervos66 <sloddervos66@gmail.com>
2024-05-05 15:18:52 +0200

    algorithm now works, added hc functionality

--- app.js
+++ app.js
@@ -1,20 +1,75 @@
 const module = (() => {
     // Function to check the input fields with the word
-    const _checkInputs = (inputFields, word) => {
-        const inputFieldsArray = Array.from(inputFields)
+    const _checkInputs = (inputFields, word, hcSwitch) => {
+        const inputFieldsArray = Array.from(inputFields);
         const guessedWord = inputFieldsArray.map(inputField => inputField.value).join('');
 
+        const wordObjectArray = [];
+        const inputObjectArray = [];
+
+        stringCharactersInArray(word, wordObjectArray);
+        stringCharactersInArray(guessedWord, inputObjectArray);
+
+        const allCorrectLettersIndex = [];
+        const wrongOrIncorrectLettersIndex = [];
+
         for (let i = 0; i < word.length; i++) {
-            if (guessedWord[i] === word[i]) {
-                inputFields[i].classList.add('CorrectPlace');
-            } else if (word.includes(guessedWord[i])) {
-                inputFields[i].classList.add('WrongPlace');
+            if (word[i] === guessedWord[i]) {
+                allCorrectLettersIndex.push(i);
             } else {
-                inputFields[i].classList.add('WrongLetter');
+                wrongOrIncorrectLettersIndex.push(i);
+            } 
+
+            let correctPlace = '';
+            let wrongPlace = '';
+            let wrongLetter = '';
+
+            if (hcSwitch.checked) {
+                correctPlace = 'hcCorrectPlace';
+                wrongPlace = 'hcWrongPlace';
+                wrongLetter = 'hcWrongLetter';
+            } else {
+                correctPlace = 'CorrectPlace';
+                wrongPlace = 'WrongPlace';
+                wrongLetter = 'WrongLetter';
             }
 
-            console.log(inputFields[i].classList);
+            allCorrectLettersIndex.forEach(index => {
+                const letterOfIndex = word[index];
+                const letterObject = wordObjectArray.find(obj => obj.letter === letterOfIndex);
+    
+                inputFields[index].classList.add(correctPlace);
+    
+                letterObject.amount--;
+            });
+    
+            wrongOrIncorrectLettersIndex.forEach(index => {
+                const letterOfIndex = guessedWord[index];
+                const letterObject = wordObjectArray.find(obj => obj.letter === letterOfIndex);
+    
+                if (letterObject.amount >= 1) {
+                    inputFields[index].classList.add(wrongPlace);
+    
+                    letterObject.amount--;
+                } else if (inputFields[index].classList.contains('className') && !(inputFields[index].classList.contains(correctPlace) || inputFields[index].classList.contains(wrongPlace))){
+                    inputFields[index].classList.add(wrongLetter);
+                }
+            });
         }
+    };
+
+    // Function to put characters of a string into an array
+    const stringCharactersInArray = (str, array) => {
+        str.split('').forEach(character => {
+            const index = array.findIndex(item => item.letter === character);
+            if (index !== -1) {
+                array[index].amount++;
+            } else {
+                array.push(
+                    { letter: character, amount: 1 }
+                );
+            }
+        });
     }
 
     // Function to generate a random 5 letter word
@@ -117,11 +172,95 @@ const module = (() => {
         }
     }
 
+    // Change colours depending on Hc or not
+    const _changeToHcOrBack = (inputFields, howItWorksArray) => {
+        inputFields.forEach(inputField => {
+            inputField.forEach(actualInputField => {
+                const classNames = actualInputField.classList.value.split(' ');
+    
+                // Iterate through each class
+                classNames.forEach(className => {
+                    switch (className) {
+                        case 'hcCorrectPlace':
+                            actualInputField.classList.remove('hcCorrectPlace');
+                            actualInputField.classList.add('CorrectPlace');
+                            break;
+                        case 'CorrectPlace':
+                            actualInputField.classList.remove('CorrectPlace');
+                            actualInputField.classList.add('hcCorrectPlace');
+                            break;
+                        case 'hcWrongPlace':
+                            actualInputField.classList.remove('hcWrongPlace');
+                            actualInputField.classList.add('WrongPlace');
+                            break;
+                        case 'WrongPlace':
+                            actualInputField.classList.remove('WrongPlace');
+                            actualInputField.classList.add('hcWrongPlace');
+                            break;
+                        case 'hcWrongLetter':
+                            actualInputField.classList.remove('hcWrongLetter');
+                            actualInputField.classList.add('WrongLetter');
+                            break;
+                        case 'WrongLetter':
+                            actualInputField.classList.remove('WrongLetter');
+                            actualInputField.classList.add('hcWrongLetter');
+                            break;
+                    }
+                });
+            });
+        });  
+        
+        howItWorksArray.forEach(element => {
+            const classNames = element.classList.value.split(' ');
+    
+                // Iterate through each class
+                classNames.forEach(className => {
+                    switch (className) {
+                        case 'hcCorrectPlace':
+                            element.classList.remove('hcCorrectPlace');
+                            element.classList.add('CorrectPlace');
+                            break;
+                        case 'CorrectPlace':
+                            element.classList.remove('CorrectPlace');
+                            element.classList.add('hcCorrectPlace');
+                            break;
+                        case 'hcWrongPlace':
+                            element.classList.remove('hcWrongPlace');
+                            element.classList.add('WrongPlace');
+                            break;
+                        case 'WrongPlace':
+                            element.classList.remove('WrongPlace');
+                            element.classList.add('hcWrongPlace');
+                            break;
+                        case 'hcWrongLetter':
+                            element.classList.remove('hcWrongLetter');
+                            element.classList.add('WrongLetter');
+                            break;
+                        case 'WrongLetter':
+                            element.classList.remove('WrongLetter');
+                            element.classList.add('hcWrongLetter');
+                            break;
+                    }
+                });
+        });
+    }
+
+    // Function to add listElements to an array
+    const _listElementsToArray = (listElements, inputFields) => {
+        listElements.forEach(element => {
+            const listOfInputs = element.querySelectorAll('input');
+
+            inputFields.push(listOfInputs);
+        });
+    }
+
     return {
         generate5Letters: _generate5Letters,
         checkInputs: _checkInputs,
         addInputFields: _addInputFields,
         disableInputFields: _disableInputFields,
+        changeToHcOrBack: _changeToHcOrBack,
+        listElementsToArray: _listElementsToArray,
         getCookie: _getCookie,
         setCookie: _setCookie
     };
@@ -131,6 +270,14 @@ const formList1 = document.getElementById('formList1');
 const formList2 = document.getElementById('formList2');
 const word = document.getElementById('word');
 const submitButton = document.getElementById('submitButton');
+const dmSwitch = document.getElementById('dmSwitch');
+const hcSwitch = document.getElementById('hcSwitch');
+const hiwWrongL = document.getElementById('hiwWrongL');
+const hiwWrongP = document.getElementById('hiwWrongP');
+const hiwCorrectL = document.getElementById('hiwCorrectL');
+
+// Array that contains the how it works divs
+const howItWorksDiv = [hiwWrongL, hiwWrongP, hiwCorrectL];
 
 const amountOfLetters = 5;
 const amountOfMaxTries = 10;
@@ -146,7 +293,8 @@ if (!module.getCookie('dailyWord')) {
     module.setCookie('dailyWord', module.generate5Letters(amountOfLetters), 1);
 } 
 
-word.innerHTML = module.getCookie('dailyWord');
+// word.innerHTML = module.getCookie('dailyWord');
+word.innerHTML = 'fazaa';
 
 const listElements1 = [];
 const listElements2 = [];
@@ -174,7 +322,7 @@ for (let i = 0; i < allListElements.length; i++) {
 
 submitButton.addEventListener('click', () => {
     const inputFields = listElements1[amountOfTries].querySelectorAll('input');
-    module.checkInputs(inputFields, word.textContent);  
+    module.checkInputs(inputFields, word.textContent, hcSwitch);  
     // amountOfTries++;
 
     // module.setCookie('amountOfTries', amountOfTries, 1);
@@ -183,7 +331,6 @@ submitButton.addEventListener('click', () => {
     //     let listElement = allListElements[i];
     //     let formList = allFormLists[i];
 
-    //     module.addInputFields(listElement, formList);
     //     module.disableInputFields(listElement, amountOfTries);
     // }
 
@@ -195,14 +342,34 @@ submitButton.addEventListener('click', () => {
 //Make the settings button display the settings menu
 function settingsMenu() {
     document.getElementById("settingsMenu").classList.toggle("settingsShown");
-  }
+}
 
-  document.addEventListener("click", function(event) {
-    var settingsMenu = document.getElementById("settingsMenu");
-    var settingsIcon = document.getElementById("settingsIcon");
+document.addEventListener("click", (event) => {
+    const settingsMenu = document.getElementById("settingsMenu");
+    const settingsIcon = document.getElementById("settingsIcon");
 
     if (!settingsMenu.contains(event.target) && event.target !== settingsIcon) {
         settingsMenu.classList.remove("settingsShown");
+    }
+}); 
+
+hcSwitch.addEventListener('change', () => {
+    if (hcSwitch.checked) {
+        const allInputFields = [];
+
+        allListElements.forEach(listElement => {
+            module.listElementsToArray(listElement, allInputFields);
+        });
+
+        module.changeToHcOrBack(allInputFields, howItWorksDiv);
+    } else {
+        const allInputFields = [];
+
+        allListElements.forEach(listElement => {
+            module.listElementsToArray(listElement, allInputFields);
+        });
+
+        module.changeToHcOrBack(allInputFields, howItWorksDiv);
     }
 });
 
